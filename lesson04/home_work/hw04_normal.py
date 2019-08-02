d416f798a050b935dd38ecd90aff3074e7523da1
ikchit <ikchit@ya.ru>
2019-08-02 12:03:43 +0300

    Выполнено не полностью 2 из 3 lesson 4 normal

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,6 +3,56 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+import re, os, random
+
+
+def check_list(symbol):
+    str_ = 'qwertyuiopasdfghjklzxcvbnm'
+    for j in str_:
+        if symbol == j:
+            return True
+            break
+
+
+def join_list():
+    b = []
+    a = []
+    for i in range(len(line)):
+        if check_list(line[i]):
+            a = a + list(line[i])
+        elif i == len(line) - 1:
+            b = b + [''.join(a)]
+        else:
+            if a != []:
+                b = b + [''.join(a)]
+            a.clear()
+    return b
+
+
+def find_AA():
+    list_a = []
+    i = 2
+    while i < len(line_2) - 3:
+        for j in range(i, len(line_2) - 3):
+            if not check_list(line_2[j]):
+                # print(j)
+                continue
+
+            else:
+                if check_list(line_2[i-1]) and check_list(line_2[i-2]):
+                    if not check_list(line_2[j-1]) and not check_list(line_2[j-2]):
+                        if line_2[i:j-2] != '':
+                            list_a.append(line_2[i:j-2])
+                i += j - i
+                break
+        i += 1
+
+    print(list_a)
+
+
+def select(r, xs):
+    return [m.group() for m in (re.match(r, x) for x in xs) if m]
+
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -21,6 +71,12 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
 
+print('Задание-1 \n Способ без re:')
+print(join_list())
+
+print('Задание-1 \n Способ с re:')
+result = re.findall(r'([a-z]+)', line)
+print(result)
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -44,10 +100,24 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+print('Задание-2 \n Способ с re:')
+result = [str(i)[2:-2] for i in re.findall(r'([a-z]{2}[A-Z]+[A-Z]{2})', line_2)]
+print(result)
 
+print('Задание-2 \n Способ без re:')
+find_AA()
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print('Задание-3')
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"file.txt")
+with open(file, "a", encoding='utf-8') as f:
+    a = []
+    for i in range(2499):
+        a.append(str(random.randint(0, 9)))
+    f.write(''.join(a))
+num = -1
+col = 0
